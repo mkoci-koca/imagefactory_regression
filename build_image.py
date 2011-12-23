7fc09f41381467540a305b7406be3b6884fda91c
Martin Koci <mkoci@redhat.com>
2011-12-23 10:06:06 +0100

    new version of the big test build_image.py

--- build_image.py
+++ build_image.py
@@ -124,8 +124,9 @@ class TestResult(object):
         </install>
         <rootpw>redhat</rootpw>
       </os>
+      %s
     </template>
-    %s
+    
     """ % (distro, version, arch, installtype, installtype, isourlstr, installtype, templatesetup)
         return tdlxml
         
@@ -183,13 +184,13 @@ class TestResult(object):
             
     def __runTestAeolusCLI(self, args):
         global temporaryfile
-        (distro, version, arch, installtype, isourlstr, targetim) = args
+        (distro, version, arch, installtype, isourlstr, targetim, templatesetup) = args
     #lets clean the logs so there is no obsolete records in it.     
         print "Clearing log file for Image Factory"
         os.system("> " + LogFileIF)
         print "Clearing log file for Image Warehouse"
         os.system("> " + LogFileIWH)
-        tdlxml = self.__getTemplate(distro, version, arch, installtype, isourlstr, targetim)
+        tdlxml = self.__getTemplate(distro, version, arch, installtype, isourlstr, targetim, templatesetup)
         os.system("echo \""+tdlxml+"\" > "+temporaryfile)
         print "See the testing template"
         print "======================================================"
@@ -337,129 +338,6 @@ def bodyTest():
                         isourlstrvar = "http://download.devel.redhat.com/nightly/latest-RHEL6.1/6/Server/%s/iso/RHEL6.1-20110510.1-Server-%s-DVD1.iso" % (arch, arch)
                     expectSuccess("RHEL6", "1", arch, installtype, isourlstrvar , targetimage, templatesetup)
     
-    '''
-    # bad distro
-    expectFail("foo", "1", "i386", "url")
-    # bad installtype
-    expectFail("Fedora", "14", "i386", "dong")
-    # bad arch
-    expectFail("Fedora", "14", "ia64", "iso")
-    '''
-    '''
-    # FedoraCore
-    for version in ["1", "2", "3", "4", "5", "6"]:
-        for arch in ["i386", "x86_64"]:
-            for installtype in ["url", "iso"]:
-                expect_success("FedoraCore", version, arch, installtype)
-    # bad FedoraCore version
-    expect_fail("FedoraCore", "24", "x86_64", "iso")
-
-    # Fedora
-    for version in ["7", "8", "9", "10", "11", "12", "13", "14", "15"]:
-        for arch in ["i386", "x86_64"]:
-            for installtype in ["url", "iso"]:
-                expect_success("Fedora", version, arch, installtype)
-    # bad Fedora version
-    expect_fail("Fedora", "24", "x86_64", "iso")
-
-    # RHL
-    for version in ["7.0", "7.1", "7.2", "7.3", "8", "9"]:
-        expect_success("RHL", version, "i386", "url")
-    # bad RHL version
-    expect_fail("RHL", "10", "i386", "url")
-    # bad RHL arch
-    expect_fail("RHL", "9", "x86_64", "url")
-    # bad RHL installtype
-    expect_fail("RHL", "9", "x86_64", "iso")
-
-    # RHEL-2.1
-    for version in ["GOLD", "U2", "U3", "U4", "U5", "U6"]:
-        expect_success("RHEL-2.1", version, "i386", "url")
-    # bad RHEL-2.1 version
-    expect_fail("RHEL-2.1", "U7", "i386", "url")
-    # bad RHEL-2.1 arch
-    expect_fail("RHEL-2.1", "U6", "x86_64", "url")
-    # bad RHEL-2.1 installtype
-    expect_fail("RHEL-2.1", "U6", "i386", "iso")
-
-    # RHEL-3
-    for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9"]:
-        for arch in ["i386", "x86_64"]:
-            expect_success("RHEL-3", version, arch, "url")
-    # bad RHEL-3 version
-    expect_fail("RHEL-3", "U10", "x86_64", "url")
-    # invalid RHEL-3 installtype
-    expect_fail("RHEL-3", "U9", "x86_64", "iso")
-
-    # RHEL-4/CentOS-4
-    for distro in ["RHEL-4", "CentOS-4"]:
-        for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9"]:
-            for arch in ["i386", "x86_64"]:
-                for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-4 version
-    expect_fail("RHEL-4", "U10", "x86_64", "url")
-
-    # RHEL-5/CentOS-5
-    for distro in ["RHEL-5", "CentOS-5"]:
-        for version in ["GOLD", "U1", "U2", "U3", "U4", "U5", "U6", "U7"]:
-            for arch in ["i386", "x86_64"]:
-                for installtype in ["url", "iso"]:
-                    expect_success(distro, version, arch, installtype)
-    # bad RHEL-5 version
-    expect_fail("RHEL-5", "U10", "x86_64", "url")
-
-    # RHEL-6
-    for version in ["0", "1"]:
-        for arch in ["i386", "x86_64"]:
-            for installtype in ["url", "iso"]:
-                expect_success("RHEL-6", version, arch, installtype)
-    # bad RHEL-6 version
-    expect_fail("RHEL-6", "U10", "x86_64", "url")
-
-    # Debian
-    for version in ["5", "6"]:
-        for arch in ["i386", "x86_64"]:
-            expect_success("Debian", version, arch, "iso")
-    # bad Debian version
-    expect_fail("Debian", "12", "i386", "iso")
-    # invalid Debian installtype
-    expect_fail("Debian", "6", "x86_64", "url")
-
-    # Windows
-    expect_success("Windows", "2000", "i386", "iso")
-    for version in ["XP", "2003", "2008", "7"]:
-        for arch in ["i386", "x86_64"]:
-            expect_success("Windows", version, arch, "iso")
-    # bad Windows 2000 arch
-    expect_fail("Windows", "2000", "x86_64", "iso")
-    # bad Windows version
-    expect_fail("Windows", "1999", "x86_64", "iso")
-    # invalid Windows installtype
-    expect_fail("Windows", "2008", "x86_64", "url")
-
-    # OpenSUSE
-    for version in ["11.0", "11.1", "11.2", "11.3", "11.4"]:
-        for arch in ["i386", "x86_64"]:
-            expect_success("OpenSUSE", version, arch, "iso")
-    # bad OpenSUSE version
-    expect_fail("OpenSUSE", "16", "x86_64", "iso")
-    # invalid OpenSUSE installtype
-    expect_fail("OpenSUSE", "11.4", "x86_64", "url")
-
-    # Ubuntu
-    for version in ["6.06", "6.06.1", "6.06.2", "6.10", "7.04", "7.10", "8.04",
-                    "8.04.1", "8.04.2", "8.04.3", "8.04.4", "8.10", "9.04",
-                    "9.10", "10.04", "10.04.1", "10.04.2", "10.04.3", "10.10",
-                    "11.04", "11.10"]:
-        for arch in ["i386", "x86_64"]:
-            expect_success("Ubuntu", version, arch, "iso")
-    # bad Ubuntu version
-    expect_fail("Ubuntu", "10.9", "i386", "iso")
-    # bad Ubuntu installtype
-    expect_fail("Ubuntu", "10.10", "i386", "url")
-    
-    '''
     for onetest in alltests:
         results.append(onetest.execute())
     print "==================================================================================================================================="
